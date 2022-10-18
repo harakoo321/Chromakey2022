bdfd1f52d4fe570472c9367f974bdbfd6657eb45
harakoo321 <kouseih321@gmail.com>
2022-10-19 00:51:41 +0900

    setMaskメソッドの移植

--- Chromakey.cs
+++ Chromakey.cs
@@ -100,7 +100,7 @@ namespace Chromakey2022
 
             Mat src_img = new Mat(); //カメラ画像
             Mat mask = new Mat(); //マスク画像（緑部分を黒、それ以外を白）
-            Mat comp = new Mat(); //背景画像
+            Mat comp; //背景画像
             Mat img = new Mat(); //
             Mat flipImg = new Mat(); //
             while (flgRun)
@@ -116,7 +116,7 @@ namespace Chromakey2022
                 mat.At<double>(1, 2) += TransY;
                 Cv2.WarpAffine(src_img, src_img2, mat, src_img2.Size(), InterpolationFlags.Linear, BorderTypes.Transparent); //アフィン変換 (元画像、変換後画像、変換行列、大きさ、補完方法、ピクセル外挿方法)
 
-                //mask = setMask(src_img2);//マスクの作成
+                mask = setMask(src_img2);//マスクの作成
 
                 comp = bg[bgIndex].Clone(); //背景画像の取得（準備した画像、または、カメラ2からとってきた画像）
 
@@ -128,7 +128,7 @@ namespace Chromakey2022
                 Cv2.ImShow("ChromakeyFlip", flipImg); //反転画像
                 Cv2.ImShow("Chromakey", img); //最終的な画像
                 Mat ipl = comp; //OpenCVの画像データを管理している構造体
-                //bmp = new Bitmap(ipl.Cols, ipl.Rows, ipl.Step, System.Drawing.Imaging.PixelFormat.Format24bppRgb, ipl.Data);
+                bmp = new Bitmap(ipl.Cols, ipl.Rows, (int)ipl.Step(), System.Drawing.Imaging.PixelFormat.Format24bppRgb, ipl.Data);
 
                 Cv2.WaitKey(30);
 
@@ -140,11 +140,36 @@ namespace Chromakey2022
             cap1.Release(); cap2.Release();
         }
 
-        /*private Mat setMask(Mat src_img)
+        private Mat setMask(Mat src_img)
         {
             Mat temp = src_img.Clone();
-            //Mat image = 
-        }*/
+            Mat img = new Mat(480, 640, MatType.CV_8UC3, new Scalar(255, 255, 255));
+            //img = Scalar(255, 255, 255);
+            Cv2.MedianBlur(temp, temp, 7);
+            Cv2.CvtColor(temp, temp, ColorConversionCodes.BGR2HSV); //HSV変換
+
+
+            for (int y = 0; y < 480; y++)
+            {
+                for (int x = 0; x < 640; x++)
+                {
+                    //int a = (int)temp.Step() * y + (x * 3);
+                    Vec3b temp_px = temp.At<Vec3b>(y, x);
+                    Vec3b img_px = img.At<Vec3b>(y, x);
+                    if (temp_px[0] >= Hlow && temp_px[0] <= Hup &&
+                        temp_px[1] >= Slow && temp_px[1] <= Sup &&
+                        temp_px[2] >= Vlow && temp_px[2] <= Vup)
+                    {
+                        img_px[0] = 0x00;
+                        img_px[1] = 0x00;
+                        img_px[2] = 0x00;
+                    }
+                    img.Set<Vec3b>(y, x, img_px);
+                }
+            }
+            temp.Release();
+            return img;
+        }
 
         /*private void MarshalString(string s, )
         {
